72755543016dc293c7870a4faf87e6f040ebeb3b
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-27 15:31:51 -0300

    feat: adiciona exemplos para tipos de dados

---
+++ section-03/data-types-example/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+sbyte sbyteNumber = 100;
+Console.WriteLine(sbyteNumber);
+
+byte byteNumber = 126;
+Console.WriteLine(byteNumber);
+
+int intNumber = 1000;
+Console.WriteLine(intNumber);
+
+long longNumber = 2147483648L;
+Console.WriteLine(longNumber);
+
+bool status = true;
+Console.WriteLine(status);
+
+char gender = 'F';
+Console.WriteLine(gender);
+
+char letter = '\u0041';
+Console.WriteLine(letter);
+
+float floatNumber = 4.5f;
+Console.WriteLine(floatNumber);
+
+double doubleNumber = 4.5;
+Console.WriteLine(doubleNumber);
+
+string name = "Ana Francisca";
+Console.WriteLine(name);
+
+// O object aceita quaisquer outros tipos de dados
+object objectOne = "Taylor Jenkins Reid";
+object objectTwo = 4.5f;
+Console.WriteLine(objectOne);
+Console.WriteLine(objectTwo);
+
+int maxIntValue = int.MaxValue;
+Console.WriteLine(maxIntValue);
+
+decimal minDecimalValue = decimal.MinValue;
+Console.WriteLine(minDecimalValue);
