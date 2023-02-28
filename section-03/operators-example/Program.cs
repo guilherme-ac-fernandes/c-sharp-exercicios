0ddfff673511b9d245796606d6b5e80461eda5ed
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-28 15:33:27 -0300

    feat: adiciona exemplos operações em C#

---
+++ section-03/operators-example/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("--- Assignment Operators ---");
+int number = 10;
+Console.WriteLine(number);
+
+number += 2;
+Console.WriteLine(number);
+
+number *= 3;
+Console.WriteLine(number);
+
+string alphabet = "ABC";
+Console.WriteLine(alphabet);
+
+alphabet += "DEF";
+Console.WriteLine(alphabet);
+
+int newNumber = 2;
+--newNumber;
+Console.WriteLine(newNumber);
+
+++newNumber;
+Console.WriteLine(newNumber);
+
+Console.WriteLine("--- Implicit Conversion and Casting ---");
+
+float number1 = 34.56f;
+double number2 = number1; // Conversão implícita
+Console.WriteLine(number2);
+
+double number3 = 34.56;
+float number4 = (float)number3;
+int number5 = (int)number3;
+Console.WriteLine(number4);
+Console.WriteLine(number5);
+
+
